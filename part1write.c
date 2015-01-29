1fef926901c88875cb7c13f3df188666c7890d95
Stephen Phillips <stephi@spec16.seas.upenn.edu>
2015-01-29 17:27:27 -0500

    Finished part 1

--- part1write.c
+++ part1write.c
@@ -1,24 +1,26 @@
 /* syscall_writer.c ‐ write 50,000 characters with write*/
+#include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #define OUTPUTNAME "write.out"
+
 main()
 {
-long i;
-int fd;
-if ((fd=open(OUTPUTNAME,O_WRONLY|O_CREAT,0644)) < 0){
-fprintf(stderr,"Can't open %s. Bye.\n",OUTPUTNAME);
-exit(1);
-}
-for (i=0; i<50000; i++) { /* write 50,000 Ys with write */
-if (write(fd,"Y",1) < 1) {
-fprintf(stderr,"Can't write. Bye\n");
-exit(1);
-}
-}
-close(fd);
-exit(0);
+	long i;
+	int fd;
+	if ((fd=open(OUTPUTNAME,O_WRONLY|O_CREAT,0644)) < 0) {
+		fprintf(stderr,"Can't open %s. Bye.\n",OUTPUTNAME);
+		exit(1);
+	}
+	for (i=0; i<50000; i++) { /* write 50,000 Ys with write */
+		if (write(fd,"Y",1) < 1) {
+			fprintf(stderr,"Can't write. Bye\n");
+			exit(1);
+		}
+	}
+	close(fd);
+	exit(0);
 }
 
