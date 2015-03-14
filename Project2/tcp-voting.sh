5335d390d92f669330357d628293bfa04288fbfe
Stephen Phillips <chengdu.scp@gmail.com>
2015-03-14 13:47:26 -0400

    Made testing files for parts 1 and 2

--- Project2/tcp-voting.sh
+++ Project2/tcp-voting.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
-touch testudp.test
-./add-voter-tcp 127.0.0.1 8269 1 >> testtcp.test
-./add-voter-tcp 127.0.0.1 8269 2 >> testtcp.test
-./vote-tcp 127.0.0.1 8269 bill 1 >> testtcp.test
-./vote-tcp 127.0.0.1 8269 bob 2 >> testtcp.test
+# Start server
+./server-tcp &
+SERVERPID=$!
 
+# Add voter tests
+./add-voter-tcp 127.0.0.1 8269 1 
+./add-voter-tcp 127.0.0.1 8269 2 
+./add-voter-tcp 127.0.0.1 8269 1 
+
+# Voting tests
+./vote-tcp 127.0.0.1 8269 bill 1 
+./vote-tcp 127.0.0.1 8269 bob 2 
+./vote-tcp 127.0.0.1 8269 bill 2 
+
+# Candidate tests
 ./list-candidates-tcp 127.0.0.1 8269
-./vote-count-tcp 127.0.0.1 8269 bill >> testtcp.test
-./vote-count-tcp 127.0.0.1 8269 bob >> testtcp.test
-./vote-count-tcp 127.0.0.1 8269 joe >> testtcp.test
+./vote-count-tcp 127.0.0.1 8269 bill 
+./vote-count-tcp 127.0.0.1 8269 bob 
+./vote-count-tcp 127.0.0.1 8269 joe 
+
+# Zeroize tests
+./zeroize-tcp 127.0.0.1 8269
+
+echo
+echo Zeroize
+echo
+
+# Add voter tests
+./add-voter-tcp 127.0.0.1 8269 1 
+./add-voter-tcp 127.0.0.1 8269 2 
+./add-voter-tcp 127.0.0.1 8269 1 
+
+# Voting tests
+./vote-tcp 127.0.0.1 8269 bill 1 
+./vote-tcp 127.0.0.1 8269 bob 2 
+
+# Candidate tests
+./list-candidates-tcp 127.0.0.1 8269
+./vote-count-tcp 127.0.0.1 8269 bill 
+./vote-count-tcp 127.0.0.1 8269 bob 
+./vote-count-tcp 127.0.0.1 8269 joe 
 
 
-
+if kill $SERVERPID; then { echo; echo Server finished; } fi
 
--- Project2/udp-voting.sh
+++ Project2/udp-voting.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
-touch testudp.test
-./add-voter-udp 127.0.0.1 8269 1 >> testudp.test
-./add-voter-udp 127.0.0.1 8269 2 >> testudp.test
-./vote-udp 127.0.0.1 8269 bill 1 >> testudp.test
-./vote-udp 127.0.0.1 8269 bob 2 >> testudp.test
+# Start server
+./server-udp &
+SERVERPID=$!
 
+# Add voter tests
+./add-voter-udp 127.0.0.1 8269 1 
+./add-voter-udp 127.0.0.1 8269 2 
+./add-voter-udp 127.0.0.1 8269 1 
+
+# Voting tests
+./vote-udp 127.0.0.1 8269 bill 1 
+./vote-udp 127.0.0.1 8269 bob 2 
+./vote-udp 127.0.0.1 8269 bill 2 
+
+# Candidate tests
 ./list-candidates-udp 127.0.0.1 8269
-./vote-count-udp 127.0.0.1 8269 bill >> testudp.test
-./vote-count-udp 127.0.0.1 8269 bob >> testudp.test
-./vote-count-udp 127.0.0.1 8269 joe >> testudp.test
+./vote-count-udp 127.0.0.1 8269 bill 
+./vote-count-udp 127.0.0.1 8269 bob 
+./vote-count-udp 127.0.0.1 8269 joe 
+
+# Zeroize tests
+./zeroize-udp 127.0.0.1 8269 
+
+echo
+echo Zeroize
+echo
+
+# Add voter tests
+./add-voter-udp 127.0.0.1 8269 1 
+./add-voter-udp 127.0.0.1 8269 2 
+./add-voter-udp 127.0.0.1 8269 1 
+
+# Voting tests
+./vote-udp 127.0.0.1 8269 bill 1 
+./vote-udp 127.0.0.1 8269 bob 2 
+
+# Candidate tests
+./list-candidates-udp 127.0.0.1 8269
+./vote-count-udp 127.0.0.1 8269 bill 
+./vote-count-udp 127.0.0.1 8269 bob 
+./vote-count-udp 127.0.0.1 8269 joe 
 
 
-
+if kill $SERVERPID; then { echo; echo Server finished; } fi
 
